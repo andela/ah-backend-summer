ebaa139da814780eeac6d2b5f3774142eb5f6216
Paul Balitema <paulbalitema@Pauls-MacBook-Pro.local>
2019-01-28 17:08:30 +0300

    chore: upgrade from Django 1.10 to Django 2.1.5
    - update requirements.txt file
    - remove regex for urls. use path and include for urls
    - declare app_name inside the urls file
    - add names to app urls
    
    [Starts #163383162]

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
 )
-
+app_name = "authentication"
 urlpatterns = [
-    url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
-    url(r'^users/?$', RegistrationAPIView.as_view()),
-    url(r'^users/login/?$', LoginAPIView.as_view()),
+    path('user/', UserRetrieveUpdateAPIView.as_view(), name='retrieve_update'),
+    path('users/', RegistrationAPIView.as_view(), name='register'),
+    path('users/login/', LoginAPIView.as_view(), name='login'),
 ]
