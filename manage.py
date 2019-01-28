b6a1cf3d4cbd14590a312a75417b0cdf591d5801
Lincoln Karuhanga <karuhanga475@gmail.com>
2019-01-28 21:37:40 +0300

    chore: Setup application database configurations
    - setup application to use PostgreSQL as backend engine
    - update settings to use settings module with appropriate environment configurations
    - make pending migrations
    - add staging environment to settings configuration

--- manage.py
+++ manage.py
@@ -3,7 +3,13 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings")
+    environment = os.environ.get('ENVIRONMENT', 'production')
+    if environment == 'development':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings.development")
+    elif environment == 'staging':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings.staging")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings.production")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
