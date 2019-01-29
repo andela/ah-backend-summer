ed8cc40a918eef28c5e2cc0a3e389a878752e024
Victor Nomwesigwa <nvbethany479@gmail.com>
2019-01-29 12:37:22 +0300

    chore(Unit tests): implement tests for user login
    - Test user login passes with correct user credentials
    - Test user login fails when no email is provided
    - Test user login fails when no password is provided
    - Test user login fails when user is not registered
    - Test user login fails with invalid email structure
    - Test user login fails when password provided is short
    - Test user login fails when no email and password are provided
    
    [Starts #163383158]

--- authors/apps/authentication/tests/base_class.py
+++ authors/apps/authentication/tests/base_class.py
@@ -1,5 +1,6 @@
 """Test base class file containing setup"""
 import json
+from .test_data import valid_register_data_2
 
 from rest_framework.test import APITestCase, APIClient
 
@@ -11,3 +12,10 @@ class BaseTest(APITestCase):
         self.client = APIClient()
 
         self.url_register = reverse('authentication:register')
+        self.url_login = reverse('authentication:login')
+
+    def register_user(self):
+        self.client.post(self.url_register,
+                         data=json.dumps(
+                             valid_register_data_2),
+                         content_type='application/json')
--- authors/apps/authentication/tests/test_data.py
+++ authors/apps/authentication/tests/test_data.py
@@ -7,6 +7,28 @@ valid_register_data = {
     }
 }
 
+valid_register_data_2 = {
+    'user': {
+        'email': 'admin@email.com',
+        'username': 'admin',
+        'password': 'pass1234'
+    }
+}
+
+valid_login_data = {
+    'user': {
+        'email': 'abc@abc.com',
+        'password': 'ia83na.JS'
+    }
+}
+
+valid_login_data_2 = {
+    'user': {
+        'email': 'admin@email.com',
+        'password': 'pass1234'
+    }
+}
+
 register_short_password = {
     'user': {
         'email': 'abc@abc.com',
@@ -47,4 +69,41 @@ register_invalid_email = {
         'username': 'abc123',
         'password': 'ia83na.JS'
     }
+}
+
+login_no_email = {
+    'user': {
+        'password': 'pass1234'
+    }
+}
+
+login_no_password = {
+    'user': {
+        'email': 'admin@email.com'
+    }
+}
+
+login_invalid_email = {
+    'user': {
+        'email': 'admin@.com',
+        'password': 'pass1234'
+    }
+}
+
+login_unregistered_email = {
+    'user': {
+        'email': 'admin1234@email.com',
+        'password': 'pass1234'
+    }
+}
+
+login_invalid_password = {
+    'user': {
+        'email': 'admin@email.com',
+        'password': 'pass1234fwfw'
+    }
+}
+
+login_no_email_password = {
+    'user': {}
 }
