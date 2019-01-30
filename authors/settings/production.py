d8473881e43a3148b9a3f3a11483513b8597540d
Karuhanga Lincoln <karuhanga475@gmail.com>
2019-01-30 09:47:08 +0300

    chore: Setup application database configurations (#2)
    - setup application to use PostgreSQL as backend engine
    - update settings to use settings module with appropriate environment configurations
    - make pending migrations
    - add staging environment to settings configuration

---
+++ authors/settings/production.py
@@ -0,0 +1,13 @@
+import dj_database_url
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(env='DATABASE_URL', engine='django.db.backends.postgresql')
+}
--- authors/wsgi.py
+++ authors/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings")
+environment = os.environ.get('ENVIRONMENT', 'production')
+if environment == 'development':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings.development")
+elif environment == 'staging':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings.staging")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings.production")
 
 application = get_wsgi_application()
