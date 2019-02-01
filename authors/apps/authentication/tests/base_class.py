6560d66d3e6db53eb0a3b06f8a422f4b93eeb57a
fahad <fahdjamy2@gmail.com>
2019-02-01 08:07:49 +0300

    feat(User JWT): Generate User JWT
    - write a custom authentication base_class
    - over write the authentication method to validate a Bearer Token
    - add a generate token method in the user class models
    - add a token field in the login and registration serializers
    - omit testing the staging in coveralls
    - write tests for the User models
    - write tests for the user login/register endpoints
    
    [Starts #163383172]

--- authors/apps/authentication/tests/base_class.py
+++ authors/apps/authentication/tests/base_class.py
@@ -13,6 +13,7 @@ class BaseTest(APITestCase):
 
         self.url_register = reverse('authentication:register')
         self.url_login = reverse('authentication:login')
+        self.url_user_detail = reverse('authentication:retrieve_update')
 
     def register_test_user(self):
         self.client.post(self.url_register,
